98a351691b78741a5529c34e5ed39d44e26e73a9
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-08-18 06:49:27 +0000

    update:Add new stuff

--- question_generator.py
+++ question_generator.py
@@ -1,23 +1,29 @@
 import json
 from os import system
 from random import choice
+from statistics import correlation
 from tqdm import tqdm
+from itertools import islice
 
-
-pass_file_names = {"1": "./questions/eng_econs_1.json"}
+pass_file_names = {"1": "./questions/eng_econs_1.json", "2":"./questions/dsp_end_of_sem.json", "3":"./questions/eng_econs_2015.json"}
 
 if __name__ == "__main__":
     system("cls|clear")
 
-    print("Select a set of questions to go through: \n1. Engineering Econs 1")
+    print("Select a set of questions to go through: \n1. Engineering Econs 1\n2. Digital Signal Processing (End of Semester)\n3. Engineering Econs 2015 (34)")
     passco_number = input("Choice: ")
 
     with open(pass_file_names[passco_number], "r") as file:
         data = json.load(file)
+    
+    start = int(input("Start: "))
+    stop = int(input("Stop: "))
 
-    questions = data["questions"]
-    possible_answers = data["possible_answers"]
-    answer = data["answers"]
+    questions = {key:value for key, value in  data["questions"].items() if start <= int(key) <= stop}  #[start:stop+1]
+    possible_answers = {key:value for key, value in  data["possible_answers"].items() if start <= int(key) <= stop} #[start:stop+1]
+    answer = {key:value for key, value in  data["answers"].items() if start <= int(key) <= stop} #[start:stop+1]
+
+    correct_answers = 0
 
     system("cls|clear")
 
@@ -30,7 +36,10 @@ if __name__ == "__main__":
             print(possible_answers[random_question_number])
             input()
             print(f"Answer: {answer[random_question_number]}")
-            input()
+            answer_is_correct = input()
+            if answer_is_correct == "c":
+                correct_answers += 1
+
 
             del questions[random_question_number]
             system("cls|clear")
@@ -40,3 +49,4 @@ if __name__ == "__main__":
         print("Completed session successfully")
     finally:
         system("cls|clear")
+        print(f"You scored {correct_answers}/{len(possible_answers)}")
