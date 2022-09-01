03620f4964ffa98dbf17fa7772a337dbb1ba7dab
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-09-01 21:48:33 +0000

    A lot of stuff

--- qa_generator.py
+++ qa_generator.py
@@ -5,12 +5,12 @@ from statistics import correlation
 from tqdm import tqdm
 from itertools import islice
 
-pass_file_names = {"1": "./questions/eng_econs_1.json", "2":"./questions/dsp_end_of_sem.json", "3":"./questions/eng_econs_2015.json", "4": "./questions/management_1.json"}
+pass_file_names = {"1": "./questions/management_1.json"}
 
 if __name__ == "__main__":
     system("cls|clear")
 
-    print("Select a set of questions to go through: \n1. Engineering Econs 1\n2. Digital Signal Processing (End of Semester)\n3. Entrepreneurship 2015 (87)\n4. Management 1 (20)")
+    print("Select a set of questions to go through: \n1. Management & Entrepreneurship")
     passco_number = input("Choice: ")
 
     with open(pass_file_names[passco_number], "r") as file:
@@ -20,7 +20,6 @@ if __name__ == "__main__":
     stop = int(input("Stop: "))
 
     questions = {key:value for key, value in  data["questions"].items() if start <= int(key) <= stop}  #[start:stop+1]
-    possible_answers = {key:value for key, value in  data["possible_answers"].items() if start <= int(key) <= stop} #[start:stop+1]
     answer = {key:value for key, value in  data["answers"].items() if start <= int(key) <= stop} #[start:stop+1]
 
     correct_answers = 0
@@ -28,18 +27,15 @@ if __name__ == "__main__":
     system("cls|clear")
 
     try:
-        for i in tqdm(range(len(possible_answers)), desc="Percentage Complete"):
+        for i in tqdm(range(len(questions)), desc="Percentage Complete"):
             random_question_number = choice(list(questions.keys()))
             print(f"\n-----Question {random_question_number}-----")
             print(questions[random_question_number])
-            input()
-            print(possible_answers[random_question_number])
             input()
             print(f"Answer: {answer[random_question_number]}")
             answer_is_correct = input()
             if answer_is_correct == "":
                 correct_answers += 1
-
 
             del questions[random_question_number]
             system("cls|clear")
@@ -49,4 +45,4 @@ if __name__ == "__main__":
         print("Completed session successfully")
     finally:
         system("cls|clear")
-        print(f"You scored {correct_answers}/{len(possible_answers)}")
+        print(f"You scored {correct_answers}/{len(answer)}")
